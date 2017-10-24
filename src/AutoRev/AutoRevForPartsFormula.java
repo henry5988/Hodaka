48d7a260be804c5bfd4adbedce6b1685021e2b3c
William Huang <shsidforever@gmail.com>
2017-10-24 15:36:36 +0800

    also fixed null check and other minor detail

--- src/AutoRev/AutoRevForPartsFormula.java
+++ src/AutoRev/AutoRevForPartsFormula.java
@@ -1,11 +1,8 @@
 package AutoRev;
 
 import com.agile.api.*;
-import com.agile.px.EventActionResult;
-import com.agile.px.IEventAction;
-import com.agile.px.IEventInfo;
-import com.agile.px.IWFChangeStatusEventInfo;
-
+import com.agile.px.*;
+import com.anselm.plm.utilobj.LogIt;
 import java.util.Iterator;
 
 /**
@@ -13,13 +10,21 @@ import java.util.Iterator;
  */
 public class AutoRevForPartsFormula implements IEventAction {
     public static IAgileSession session;
+    private static LogIt logger;
     @Override
     public EventActionResult doAction(IAgileSession session, INode actionNode, IEventInfo event) {
         IWFChangeStatusEventInfo info = (IWFChangeStatusEventInfo) event;
         this.session = session;
+        try {
+            logger = new LogIt("AutoRevForPartsFormula");
+            logger.setLogFile("C:/Agile/AutoRevForPartsFormula.log");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
         try {
             IChange changeOrder = (IChange) info.getDataObject();
+            logger.log("Change Order = "+changeOrder.toString());
             ITable affectedTable = changeOrder.getTable(ChangeConstants.TABLE_AFFECTEDITEMS);
             Iterator it = affectedTable.iterator();
             while(it.hasNext()){
@@ -29,14 +34,15 @@ public class AutoRevForPartsFormula implements IEventAction {
         } catch (APIException e) {
             e.printStackTrace();
         }
-        return null;
+        return new EventActionResult(event, new ActionResult(ActionResult.STRING, "成功"));
     }
 
     private void autoRev(IRow row) throws APIException{
         String oldRev = (String) row.getValue(ChangeConstants.ATT_AFFECTED_ITEMS_OLD_REV);
-        if(oldRev==null)
+        if(oldRev=="")
             row.setValue(ChangeConstants.ATT_AFFECTED_ITEMS_NEW_REV,1);
         else
             row.setValue(ChangeConstants.ATT_AFFECTED_ITEMS_NEW_REV,Integer.parseInt(oldRev)+1);
+        logger.log(1,"New Rev for "+row.getReferent().toString()+" is "+row.getValue(ChangeConstants.ATT_AFFECTED_ITEMS_NEW_REV));
     }
 }
